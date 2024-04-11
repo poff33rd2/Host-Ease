a75f0335d9649e2446fe81e64fe88332e2904a21
poff33rd2 <149134778+poff33rd2@users.noreply.github.com>
2024-04-11 12:05:28 -0500

    Branding swap - Logo and font
    This project is solely for testing and Zeta to beta testing.

--- app/ui/acme-logo.tsx
+++ app/ui/acme-logo.tsx
@@ -1,13 +1,21 @@
-import { GlobeAltIcon } from '@heroicons/react/24/outline';
-import { lusitana } from '@/app/ui/fonts';
+import Image from 'next/image';
+import { inter } from '@/app/ui/fonts';
+import React from 'react';
 
 export default function AcmeLogo() {
   return (
     <div
-      className={`${lusitana.className} flex flex-row items-center leading-none text-white`}
+      className={`${inter.className} flex flex-row items-center leading-none text-white`}
     >
-      <GlobeAltIcon className="h-12 w-12 rotate-[15deg]" />
-      <p className="text-[44px]">Acme</p>
+      {/* <GlobeAltIcon className="h-12 w-12 rotate-[15deg]" /> */}
+      <Image 
+          src='/HE-red-logo.png'
+          width={50} 
+          height={50} 
+          className='block md:hidden'
+          alt='Screenshots of the dashboard project showing mobile version'
+          />
+      <p className="text-[18px] pl-3">No more waiting...</p>
     </div>
   );
 }
